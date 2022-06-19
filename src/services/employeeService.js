82f0c2143a31247d092db2dd498ad187f74bbedc
Malaka Silva <malakasilva43@gmail.com>
2022-06-19 11:31:02 +0530

    add env

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:8080";
+const baseURL = process.env.REACT_APP_BASE_URL;
 
 export async function getEmployeeData() {
   return await axios.get(`${baseURL}/employees/`);
