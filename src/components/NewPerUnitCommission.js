114b136d082a0e08f87b08d308b8029207e20f2c
Malaka Silva <malakasilva43@gmail.com>
2022-06-21 00:49:57 +0530

    add per unit com

---
+++ src/components/NewPerUnitCommission.js
@@ -0,0 +1,176 @@
+import React, { useEffect } from "react";
+import {
+  Drawer,
+  Form,
+  Col,
+  Row,
+  Input,
+  Button,
+  Space,
+  Select,
+  Tooltip,
+  InputNumber,
+} from "antd";
+import { useStoreActions, useStoreState } from "easy-peasy";
+
+function NewPerUnitCommission(props) {
+  const { visible, onClose } = props;
+
+  const { getCommissionsByPayCycleThunk, addCommissionsThunk, actionDrawer } =
+    useStoreActions((actions) => actions.perUnitCommissions);
+  const { getEmployeesThunk } = useStoreActions((actions) => actions.employees);
+  const { employees, isEmpLoading } = useStoreState((state) => state.employees);
+
+  const { Option } = Select;
+
+  useEffect(() => {
+    getEmployeesThunk(); // eslint-disable-next-line
+  }, []);
+
+  const toggleDrawer = () => {
+    actionDrawer();
+  };
+
+  const onFinish = (values) => {
+    addCommissionsThunk(values);
+    toggleDrawer();
+    setTimeout(() => {
+      getCommissionsByPayCycleThunk();
+    }, 5000);
+  };
+
+  const getEmployeeSeletor = () => {
+    if (!isEmpLoading) {
+      return employees.map((value, index) => {
+        return (
+          <Option value={value.employeeId} key={value._id}>
+            {value.name}
+          </Option>
+        );
+      });
+    } else {
+      return "";
+    }
+  };
+
+  const getPayCycle = () => {
+    let d = new Date();
+
+    const monthCodes = [
+      "JAN",
+      "FEB",
+      "MAR",
+      "APR",
+      "MAY",
+      "JUN",
+      "JUL",
+      "AUG",
+      "SEP",
+      "OVT",
+      "NOV",
+      "DEC",
+    ];
+
+    const year = d.getFullYear();
+
+    let code = year + monthCodes[d.getMonth()];
+    return code;
+  };
+
+  return (
+    <Drawer
+      title="New Commission"
+      width={400}
+      onClose={onClose}
+      visible={visible}
+      bodyStyle={{ paddingBottom: 80 }}
+    >
+      <Form layout="vertical" onFinish={onFinish} hideRequiredMark>
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item
+              name="name"
+              label="Commission"
+              rules={[{ required: true, message: "Please enter Commission." }]}
+            >
+              <Input placeholder="Please enter Commission" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item
+              name="employeeId"
+              label="Employee"
+              rules={[{ required: true, message: "Please select Employee." }]}
+            >
+              <Select placeholder="Please select Employee">
+                {/* <Option value="all" key="all">
+                    All Employees
+                  </Option> */}
+                {getEmployeeSeletor()}
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={17}>
+            <Form.Item
+              name="amount"
+              label="Amount"
+              rules={[{ required: true, message: "Please enter Amount." }]}
+            >
+              <Input placeholder="Please enter Amount" />
+            </Form.Item>
+          </Col>
+          <Col span={7}>
+            <Form.Item
+              name="units"
+              label="Units"
+              rules={[
+                { required: true, message: "Please enter amount of Units." },
+              ]}
+            >
+              <InputNumber min={1} defaultValue={1} />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item
+              name="payCycle"
+              label={
+                <Tooltip
+                  placement="top"
+                  title="If no value is given, current pay cycle will be selected."
+                >
+                  Pay Cycle *
+                </Tooltip>
+              }
+              rules={[{ required: true, message: "Please enter Pay Cycle" }]}
+              initialValue={getPayCycle()}
+            >
+              <Input placeholder="Please enter Pay Cycle" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item>
+              <Space>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+                <Button htmlType="button" onClick={toggleDrawer}>
+                  Cancel
+                </Button>
+              </Space>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </Drawer>
+  );
+}
+
+export default NewPerUnitCommission;
--- src/components/PerUnitCommissions.js
+++ src/components/PerUnitCommissions.js
@@ -1,9 +1,166 @@
-import React from 'react'
+import { useStoreState, useStoreActions } from "easy-peasy";
+import { Table, Space, Card, Spin, Tooltip, Button, Select } from "antd";
+import {
+  LoadingOutlined,
+  UserAddOutlined,
+  EditOutlined,
+  UserDeleteOutlined,
+} from "@ant-design/icons";
+import React, { useEffect } from "react";
+
+import NewPerUnitCommission from "./NewPerUnitCommission";
 
 function PerUnitCommissions(props) {
+  let dataPayCycle = { payCycle: "2022JUN" };
+
+  const { Option } = Select;
+
+  const { puCommissions, isPUComLoading, drawerVisible } = useStoreState(
+    (state) => state.perUnitCommissions
+  );
+  const { employees, isEmpLoading } = useStoreState((state) => state.employees);
+  const {
+    getCommissionsByUserIdPayCycleThunk,
+    getCommissionsByPayCycleThunk,
+    actionDrawer,
+  } = useStoreActions((actions) => actions.perUnitCommissions);
+  const { getEmployeesThunk } = useStoreActions((actions) => actions.employees);
+
+  const columns = [
+    {
+      title: "Commission",
+      dataIndex: "commissionName",
+      key: "commissionName",
+    },
+    {
+      title: "Pay Cycle",
+      dataIndex: "payCycle",
+      key: "payCycle",
+    },
+    {
+      title: "Units",
+      dataIndex: "units",
+      key: "units",
+    },
+    {
+      title: "Amount (Rs.)",
+      dataIndex: "amount",
+      key: "amount",
+      render: (text, record) => {
+        return <Space size="middle">{text.toFixed(2)}</Space>;
+      },
+    },
+    {
+      title: "Employee",
+      dataIndex: "employeeId",
+      render: (text, record) => (
+        <Space size="middle">{getEmployeeName(text)}</Space>
+      ),
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text, record) => (
+        <Space size="middle">
+          {<EditOutlined />}
+          {<UserDeleteOutlined />}
+        </Space>
+      ),
+    },
+  ];
+
+  useEffect(() => {
+    getEmployeesThunk();
+    getCommissionsByPayCycleThunk(dataPayCycle); // eslint-disable-next-line
+  }, []);
+
+  const getData = () => {
+    let _commissions = [];
+    puCommissions.forEach((commission) => {
+      commission["key"] = commission._id;
+      _commissions.push(commission);
+    });
+
+    return _commissions;
+  };
+
+  const handleChange = (value) => {
+    let data = {
+      payCycle: "2022JUN",
+    };
+
+    if (value === "all") {
+      getCommissionsByPayCycleThunk(dataPayCycle);
+    } else {
+      data = { ...data, userId: value };
+      getCommissionsByUserIdPayCycleThunk(data);
+    }
+  };
+
+  const getEmployeeSelector = () => {
+    return (
+      <Select
+        defaultValue="all"
+        onChange={handleChange}
+        style={{ width: 140, marginRight: "5px" }}
+      >
+        <Option value="all" key="all">
+          All Employees
+        </Option>
+        {isEmpLoading
+          ? ""
+          : employees.map((element, index) => {
+              return (
+                <Option value={element.employeeId} key={element._id}>
+                  {element.name}
+                </Option>
+              );
+            })}
+      </Select>
+    );
+  };
+
+  const getEmployeeName = (empId) => {
+    return isEmpLoading
+      ? ""
+      : employees.find((ele) => ele.employeeId === empId).name;
+  };
+
+  const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+  const toggleDrawer = () => {
+    actionDrawer();
+  };
+
   return (
-    <div>PerUnitCommissions</div>
-  )
+    <div>
+      <NewPerUnitCommission visible={drawerVisible} onClose={toggleDrawer} />
+      <Card
+        title="Commissions"
+        style={{ margin: "20px", borderRadius: "15px" }}
+        extra={
+          <div>
+            {getEmployeeSelector()}
+            <Tooltip title="New employee">
+              <Button
+                type="primary"
+                icon={<UserAddOutlined />}
+                onClick={toggleDrawer}
+              >
+                New Commission
+              </Button>
+            </Tooltip>
+          </div>
+        }
+      >
+        {isPUComLoading ? (
+          <Spin indicator={antIcon} />
+        ) : (
+          <Table columns={columns} dataSource={getData()} />
+        )}
+      </Card>
+    </div>
+  );
 }
 
-export default PerUnitCommissions
+export default PerUnitCommissions;
