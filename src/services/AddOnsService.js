10ef0a0ca3dbea77b58541ae5bd04eeb3f834d18
Malaka Silva <malakasilva43@gmail.com>
2022-06-25 12:15:25 +0530

    adjust endpoints

--- src/services/AddOnsService.js
+++ src/services/AddOnsService.js
@@ -3,11 +3,15 @@ import axios from "axios";
 const baseURL = process.env.REACT_APP_BASE_URL;
 
 export async function getAddOnsDataEmpId(empId) {
-  return await axios.get(`${baseURL}/add-ons/${empId}`);
+  return await axios.get(`${baseURL}/add-ons/by-emp/${empId}`);
+}
+
+export async function getAddOnsDataPayCycle(payCycle) {
+  return await axios.get(`${baseURL}/add-ons/by-pay-cycle/${payCycle}`);
 }
 
 export async function getAddOnsDataEmpIdPayCycle(empId, payCycle) {
-  return await axios.get(`${baseURL}/add-ons/${empId}/${payCycle}`);
+  return await axios.get(`${baseURL}/add-ons/by-emp-pay-cycle/${empId}/${payCycle}`);
 }
 
 export async function addAddOnsData(data) {
