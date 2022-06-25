cb65a24bc06b9c5a57831c266d2113328f133f04
Malaka Silva <malakasilva43@gmail.com>
2022-06-25 12:10:33 +0530

    Overview

---
+++ src/common/Overview.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Image } from "antd";
+
+function Overview() {
+  return (
+    <Image
+      width={200}
+      src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+    />
+  );
+}
+
+export default Overview;
