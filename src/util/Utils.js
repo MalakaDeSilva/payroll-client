466791c54f117f61712f1ed021a2c4bfbc47797d
Malaka Silva <malakasilva43@gmail.com>
2022-07-07 16:06:40 +0530

    getMonthYearFromPayCycle

--- src/util/Utils.js
+++ src/util/Utils.js
@@ -28,6 +28,27 @@ export function getPayCycle(year = "", month = "") {
   return code;
 }
 
+export function getMonthYearFromPayCycle(payCycle) {
+  let months = new Map();
+  months.set("JAN", "January");
+  months.set("FEB", "February");
+  months.set("MAR", "March");
+  months.set("APR", "April");
+  months.set("MAY", "May");
+  months.set("JUN", "June");
+  months.set("JUL", "July");
+  months.set("AUG", "August");
+  months.set("SEP", "September");
+  months.set("OCT", "October");
+  months.set("NOV", "Novembar");
+  months.set("DEC", "Decembar");
+
+  let year = payCycle.slice(0, 4);
+  let month = months.get(payCycle.slice(4));
+
+  return { year, month };
+}
+
 export function isObjEmpty(obj) {
   return !(() => {
     for (const i in obj) {
