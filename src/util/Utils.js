6129728f5025f2e8569ae7a79349d40e6bce276d
Malaka Silva <malakasilva43@gmail.com>
2022-07-07 19:30:56 +0530

    towrds

--- src/util/Utils.js
+++ src/util/Utils.js
@@ -57,3 +57,145 @@ export function isObjEmpty(obj) {
     return false;
   })();
 }
+
+export function inWords(num) {
+  var a = [
+    "",
+    "one ",
+    "two ",
+    "three ",
+    "four ",
+    "five ",
+    "six ",
+    "seven ",
+    "eight ",
+    "nine ",
+    "ten ",
+    "eleven ",
+    "twelve ",
+    "thirteen ",
+    "fourteen ",
+    "fifteen ",
+    "sixteen ",
+    "seventeen ",
+    "eighteen ",
+    "nineteen ",
+  ];
+  var b = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+  ];
+
+  if ((num = num.toString()).length > 9) return "overflow";
+  let n = ("000000000" + num)
+    .substr(-9)
+    .match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+  if (!n) return;
+  var str = "";
+  str +=
+    n[1] !== 0
+      ? (a[Number(n[1])] || b[n[1][0]] + " " + a[n[1][1]]) + "crore "
+      : "";
+  str +=
+    n[2] !== 0
+      ? (a[Number(n[2])] || b[n[2][0]] + " " + a[n[2][1]]) + "lakh "
+      : "";
+  str +=
+    n[3] !== 0
+      ? (a[Number(n[3])] || b[n[3][0]] + " " + a[n[3][1]]) + "thousand "
+      : "";
+  str +=
+    n[4] !== 0
+      ? (a[Number(n[4])] || b[n[4][0]] + " " + a[n[4][1]]) + "hundred "
+      : "";
+  str +=
+    n[5] !== 0
+      ? (str !== "" ? "and " : "") +
+        (a[Number(n[5])] || b[n[5][0]] + " " + a[n[5][1]]) +
+        "only "
+      : "";
+  return str;
+}
+
+export function toWords(numberInput) {
+  let oneToTwenty = [
+    "",
+    "one ",
+    "two ",
+    "three ",
+    "four ",
+    "five ",
+    "six ",
+    "seven ",
+    "eight ",
+    "nine ",
+    "ten ",
+    "eleven ",
+    "twelve ",
+    "thirteen ",
+    "fourteen ",
+    "fifteen ",
+    "sixteen ",
+    "seventeen ",
+    "eighteen ",
+    "nineteen ",
+  ];
+  let tenth = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+  ];
+
+  if (numberInput.toString().length > 7) return "overlimit";
+  console.log(numberInput);
+  //let num = ('0000000000'+ numberInput).slice(-10).match(/^(\d{1})(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+  let num = ("0000000" + numberInput)
+    .slice(-7)
+    .match(/^(\d{1})(\d{1})(\d{2})(\d{1})(\d{2})$/);
+  console.log(num);
+  if (!num) return;
+
+  let outputText =
+    num[1] != 0
+      ? (oneToTwenty[Number(num[1])] ||
+          `${tenth[num[1][0]]} ${oneToTwenty[num[1][1]]}`) + " million "
+      : "";
+
+  outputText +=
+    num[2] != 0
+      ? (oneToTwenty[Number(num[2])] ||
+          `${tenth[num[2][0]]} ${oneToTwenty[num[2][1]]}`) + "hundred "
+      : "";
+  outputText +=
+    num[3] != 0
+      ? (oneToTwenty[Number(num[3])] ||
+          `${tenth[num[3][0]]} ${oneToTwenty[num[3][1]]}`) + " thousand "
+      : "";
+  outputText +=
+    num[4] != 0
+      ? (oneToTwenty[Number(num[4])] ||
+          `${tenth[num[4][0]]} ${oneToTwenty[num[4][1]]}`) + "hundred "
+      : "";
+  outputText +=
+    num[5] != 0
+      ? oneToTwenty[Number(num[5])] ||
+        `${tenth[num[5][0]]} ${oneToTwenty[num[5][1]]} `
+      : "";
+
+  return outputText;
+}
